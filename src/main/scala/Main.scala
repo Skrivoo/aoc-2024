58424f28ead341846a376520ffee90b1088a4165
david.skrivanek <david.skrivanek@emarsys.com>
2024-12-20 15:42:09 +0100

    complete day5

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,6 +1,7 @@
 import Day1.{day1task1, day1task2}
 import Day2.{day2task1, day2task2}
 import Day3.{day3task1, day3task2}
+import Day5.{day5task1, day5task2}
 
 @main def hello(): Unit =
   println(day1task1)
@@ -9,4 +10,6 @@ import Day3.{day3task1, day3task2}
   println(day2task2)
   println(day3task1)
   println(day3task2)
+  println(day5task1)
+  println(day5task2)
 
