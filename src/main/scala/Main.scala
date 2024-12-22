83ba075c33364e8ae73dc7db2f92d1855ed38f46
Skrivoo <pedrusz96@gmail.com>
2024-12-22 21:18:42 +0100

    let's continue the journey somewhere else

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,6 +2,7 @@ import Day1.{day1task1, day1task2}
 import Day2.{day2task1, day2task2}
 import Day3.{day3task1, day3task2}
 import Day5.{day5task1, day5task2}
+import Day6.{day6task1}
 
 @main def hello(): Unit =
   println(day1task1)
@@ -12,4 +13,4 @@ import Day5.{day5task1, day5task2}
   println(day3task2)
   println(day5task1)
   println(day5task2)
-
+  println(day6task1)
